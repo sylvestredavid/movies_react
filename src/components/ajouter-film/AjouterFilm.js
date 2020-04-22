d96f46bf7c85133781a6db12830d2cc0c5e594e1
Gosuhan <david.sylvestre.lp2@gmail.com>
2020-04-22 22:05:41 +0200

    mise en page formulaire

--- src/components/ajouter-film/AjouterFilm.js
+++ src/components/ajouter-film/AjouterFilm.js
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 export default class AjouterFilm extends Component {
 
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             film: {
                 titre: '',
@@ -57,6 +57,17 @@ export default class AjouterFilm extends Component {
         })
     }
 
+    changeLiked = (e) => {
+        console.log(e.target.checked)
+        this.setState({
+            film: {
+                titre: this.state.film.titre,
+                img: this.state.film.img,
+                liked: e.target.checked
+            }
+        })
+    }
+
     render() {
         return (
             <div className="d-flex flex-row border flex-fill pt-4 p-2 justify-content-around">
@@ -67,17 +78,19 @@ export default class AjouterFilm extends Component {
                     <label htmlFor="image" className="mt-2">Image</label>
                     <input type="text" id="image" value={this.state.film.img} onChange={this.changeImg}
                            className="form-control"/>
+                    <div className="form-check mt-2">
+                        <input className="form-check-input"  type="checkbox" value={this.state.film.liked} onChange={this.changeLiked} id="liked"/>
+                        <label className="form-check-label" htmlFor="liked">J'aime</label>
+                    </div>
                     <input type="submit" value="Envoyer" className="btn btn-primary mt-2"/>
                     <p className="form-text text-muted">{this.state.message}</p>
                 </form>
                 <div className="w-25 p-2">
                     <div className="card">
-                        {
-                            this.state.film.img.length > 0 &&
-                            <img src={this.state.film.img} alt='' className="card-img-top"/>
-                        }
+                        <img src={this.state.film.img.length > 0 ? this.state.film.img : 'https://www.karriere-jet.de/skin/images/hints/no-preview-available.jpg'} alt='' className="card-img-top"/>
                         <div className="card-body">
-                            <h5 className="card-title">{this.state.film.titre}</h5>
+                            <h5 className="card-title">{this.state.film.titre.length > 0 ? this.state.film.titre : 'Titre'}</h5>
+                            <div className="favHeart" style={{'color': this.state.film.liked ? 'red': 'black'}} onClick={this.toggleLike}>❤</div>
                         </div>
                     </div>
                 </div>
